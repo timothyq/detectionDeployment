ea8e4528f832c39c1a35ea92dbaa115c8e0035a0
Qinyuanxun <qyx@Unicom.local>
2020-11-25 14:25:14 +0800

    creation

---
+++ README.md
@@ -0,0 +1,2 @@
+Walk Through:
+https://blog.cambridgespark.com/deploying-a-machine-learning-model-to-the-web-725688b851c7
---
+++ app.py
@@ -0,0 +1,13 @@
+import flask
+import pickle
+# Use pickle to load in the pre-trained model.
+with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
+    model = pickle.load(f)
+app = flask.Flask(__name__, template_folder='templates')
+@app.route('/')
+def main():
+    return(flask.render_template('main.html'))
+if __name__ == '__main__':
+    app.run()
+
+    
