3f83e8fc40927af99796c937b7d0a762718ad692
Qinyuanxun <qyx@Unicom.local>
2020-11-26 21:06:12 +0800

    2nd

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-import flask
-import pickle
-# Use pickle to load in the pre-trained model.
-with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
-    model = pickle.load(f)
-app = flask.Flask(__name__, template_folder='templates')
-@app.route('/')
-def main():
-    return(flask.render_template('main.html'))
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/home/me/Desktop/projects/flask/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    filename = 'http://127.0.0.1:5000/uploads/' + filename
+    return render_template('template.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
 if __name__ == '__main__':
     app.run()
-
-    
